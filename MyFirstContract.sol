15af82465a5871774c250377cced75ef0dd81f07
Kaycee276 <kizuaba@gmail.com>
2024-05-13 18:05:12 +0100

    I got a count, increased and also decreased it

--- MyFirstContract.sol
+++ MyFirstContract.sol
@@ -18,3 +18,30 @@ contract MyFirstContract {
     }
 
 }
+
+// Contract to write Hello World
+contract HelloWorld {
+    string public FirstMessage = 'Hello World';
+}
+
+// Contract to increase and decrease a count, also to get the maximum and minimum of a signed signedInteger
+contract counter {
+    uint256 count;
+
+    function get() public view returns (uint256){
+        return count;
+    }
+
+    // function to increase count by 1
+    function Increase() public {
+         count += 1;
+    }
+
+    // funtion to decrease count by 2
+    function Decrease() public {
+        count -= 2;
+    }
+
+    int256 public maxInt = type(int256).max;
+    int256 public minInt = type(int256).min;
+}
